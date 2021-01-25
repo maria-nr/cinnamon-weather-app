d7f6557d565308f5debe86e73fa3665c7eff1c47
maria-nr <66881646+maria-nr@users.noreply.github.com>
2021-01-25 13:28:11 +0100

    Search engine

--- src/app.js
+++ src/app.js
@@ -60,9 +60,21 @@ function displayTemperature (response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "c522524629fe11fe83aebe5bb3814efa";
-let city = "london";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  
+function search(city){
+  let apiKey = "c522524629fe11fe83aebe5bb3814efa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 
-axios.get(apiUrl).then(displayTemperature);
+}
+
+
+
+
+function handleSubmit(event){
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
